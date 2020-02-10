a8fd4e2a63618575f8b1662b6d11613e5dac115f
Ariel Calisaya <calisayaariel62@gmail.com>
2020-02-10 18:05:20 -0300

    Updates

---
+++ precache-manifest.88f89190b6ed3bcbd27ff95b218bb5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8ace8edea66454d2fe5113b49777df",
+    "url": "/SocialMedia/index.html"
+  },
+  {
+    "revision": "ad1a6b055145179ec818",
+    "url": "/SocialMedia/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f9140cf24fccae7dd2c2",
+    "url": "/SocialMedia/static/js/2.dfb6f3ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SocialMedia/static/js/2.dfb6f3ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad1a6b055145179ec818",
+    "url": "/SocialMedia/static/js/main.cc0a98f3.chunk.js"
+  },
+  {
+    "revision": "e12c1b09ec3a3520f444",
+    "url": "/SocialMedia/static/js/runtime-main.30af2d09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SocialMedia/static/media/logo.5d5d9eef.svg"
+  }
+]);
