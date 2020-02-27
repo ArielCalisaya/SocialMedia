f3f8934216f9e03ca3ba61e21314737d7b192d80
Ariel Calisaya <calisayaariel62@gmail.com>
2020-02-27 19:49:31 -0300

    test deploy 1.2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import {MuiThemeProvider} from '@material-ui/core/styles';
