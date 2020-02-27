2daa6be74c1161a92c0799604bb88516357617ef
Ariel Calisaya <calisayaariel62@gmail.com>
2020-02-27 19:58:49 -0300

    test deploy 1.4

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,7 @@ class home extends Component {
         comments: null
     };
     componentDidMount() {
-        axios.get('/comments')
+        axios.get('https://us-central1-social-media-d9c51.cloudfunctions.net/api/comments')
         .then( res => {
             console.log(res.data);
             this.setState({
