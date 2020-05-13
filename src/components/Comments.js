2a9595b6f9d6c64096fe243a73f2169ff37d5997
ArielC <calisayaariel62@gmail.com>
2020-05-13 17:46:58 -0400

    updated packages without vulneravilities

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -8,7 +8,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
- 
+
 // Icons
 import ChatIcon from '@material-ui/icons/Chat';
 import FavoriteIcon from '@material-ui/icons/Favorite'
